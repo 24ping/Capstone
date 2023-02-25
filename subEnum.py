dd6724b0028d39b3d49bade2f223eb2b7a821515
Hamza Zaher <hamzazaher@new-host-1.home>
2023-02-25 19:32:34 +0100

    enumeration using worldlist
    the program should be optimized it does 9s for 10000 and  cd /Users/hamzazaher/VSprojects/Capstone ; /usr/bin/env /usr/local/bin/python3 /Users/hamzazaher/.vscode/extensions/ms-python.python-2023.2.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 55439 -- /Users/hamzazaher/VSprojects/Capstone/GitProgress/Capstone/subEnum.py

--- subEnum.py
+++ subEnum.py
@@ -1,5 +1,6 @@
 """
 this file is used to enumurate all the subdomains of a domain name horizantanly and vertically  
+the multhread branch is used to test how fast multithreading 
 """
 # firstly i am going to enumerate vertically.
 # from dns import resolver
@@ -7,32 +8,65 @@ import dns.resolver
 # the next import is to allow me to pass arguments through the command line or exit the script
 import sys
 # subdomain and doamin are going to be taking from the command line/
+# this import below is to know how much time my program takes
+import time
+# the below import is for multithreading
+import concurrent.futures
+start_time = time.perf_counter()
 domain = 'youtube'
 tld = 'com'
 # this is going to be an array 
-subdomain_arr = ['www','mail','accounts']
-def func():
-    valid_domain = []
-    for sub_domain_s in subdomain_arr:
-        try:
-        # the line below resolves the domain name to an ip address types IPV4
-            ip_value = dns.resolver.resolve(f'{sub_domain_s}.{domain}.{tld}', 'A')
-            if ip_value:
-                print("found")
-                print(*ip_value)
-                valid_domain.append(f'{sub_domain_s}.{domain}')
-        # this line os an exception if there are no doamains detected
-        except dns.resolver.NXDOMAIN:
-            print("no subdomain ")
-            print(f'{sub_domain_s}.{domain}')
-            pass 
-        except dns.resolver.NoAnswer:
-            pass
-        except KeyboardInterrupt:
-            quit()
-    return valid_domain
+## for now i am going to comment the arr and use the wordlist file
+##subdomain_arr = ['www','mail','accounts']
+fileWL = open("/Users/hamzazaher/VSprojects/Capstone/GitProgress/Capstone/wordlist.txt","r")
+##test = fileWL.readline()
+
+def validDomain(sub_domain):
+    try:
+       ip_value = dns.resolver.resolve(f'{sub_domain}.{domain}.{tld}', 'A') 
+       if ip_value:
+                #print("found")
+                #print(*ip_value)
+                return (f'{sub_domain}.{domain}.{tld}')
+    except dns.resolver.NXDOMAIN:
+        #print("!!!!!")
+        #print("no subdomain ")
+        #print(f'{sub_domain.strip()}.{domain}.{tld}')
+        #print("!!!!!")
+        pass       
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.resolver.LifetimeTimeout:
+        pass
+    except KeyboardInterrupt:
+        quit()
+    #return (f'{sub_domain}.{domain}.{tld}')
 
 
-valid_domain = func() 
+def func(max_cores):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_cores) as executor:
+        with open('/Users/hamzazaher/VSprojects/Capstone/GitProgress/Capstone/wordlist.txt', 'r') as wordlist:
+            valid_domain = []
+            results = []
+            true_result = []
+            for test in wordlist :
+                valid_domain.append(executor.submit(validDomain, test.strip()))
+                # object_sd = executor.submit(validDomain, test.strip())
+                #valid_domain.append(object_sd.result())    
+            for valid_d in concurrent.futures.as_completed(valid_domain):
+                results.append(valid_d.result())          
+    return results
+
+tr_results=  []
+valid_domain = func(128) 
+print(valid_domain)
+print("!!!!!!!!!!")
+print(type(valid_domain[1]))
+valid_domain = list(filter(lambda x: x is not None, valid_domain))
+
 print(valid_domain)
 
+fileWL.close
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+print("Elapsed time:", elapsed_time, "seconds")
